3577ba81345b0c3fe7e4b97b3330e92aefc889b2
Bartholomeas <weivan321@gmail.com>
2022-09-18 16:00:10 +0200

    pagination preflight test

--- components/organisms/Pagination/Pagination.tsx
+++ components/organisms/Pagination/Pagination.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 interface Props {
 	currPage: number;
@@ -7,18 +8,23 @@ const Pagination = ({ currPage }: Props) => {
 	const [pages, setPages] = useState<any>({ previousPages: [], nextPages: [] });
 
 	useEffect(() => {
-		for (let i = 0; i < currPage; i++) {
+		for (let i = 1; i < currPage; i++) {
+			console.log(i);
 			setPages((prevState: any) => ({
 				...prevState,
 				previousPages: [...prevState.previousPages, i],
 			}));
 		}
-	}, [currPage]);
+	}, []);
 	console.log(pages);
 
 	return (
-		<div className='flex items-center text-font text-xs bg-rose-100 rounded'>
+		<div className='flex items-center gap-sm text-font text-xs bg-rose-100 rounded'>
 			<button className=' px-sm text font-semibold'>{'<'}</button>
+			{pages.previousPages.map((page: number) => {
+				return <button key={uuidv4()}>{page}</button>;
+			})}
+
 			<p className='font-semibold text-sm'>{currPage}</p>
 			<button className=' px-sm text font-semibold'>{'>'}</button>
 		</div>
