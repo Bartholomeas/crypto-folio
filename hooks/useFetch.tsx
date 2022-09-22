a7a6173feb3ae8120d8ff553d306b637773b3446
Bartholomeas <weivan321@gmail.com>
2022-09-22 22:46:42 +0200

    fix vercel building error with useFetch commented hook

--- hooks/useFetch.tsx
+++ hooks/useFetch.tsx
@@ -1,30 +1,29 @@
-// import { useState, useCallback, useEffect } from 'react';
-// import axios from 'axios';
-// import { data } from '../components/organisms/SparklineChart/SparklineChart';
+import { useState, useCallback, useEffect } from 'react';
+import axios from 'axios';
 
-// const useFetch = (url?: string) => {
-// 	const [data, setData] = useState({});
-// 	const [isLoading, setIsLoading] = useState(false);
-// 	const [error, setError] = useState('null');
+const useFetch = (url?: string) => {
+	const [data, setData] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState('null');
 
-// 	const sendRequest = async () => {
-// 		setIsLoading(true);
-// 		setError('null');
-// 		try {
-// 			const response = await axios(url!);
-// 			setData(response);
-// 			response;
-// 		} catch (err) {
-// 			setError('Something went wrong!');
-// 			throw new Error('Something went wrong!');
-// 		}
-// 	};
+	const sendRequest = async () => {
+		setIsLoading(true);
+		setError('null');
+		try {
+			const response = await axios(url!);
+			setData(response);
+			response;
+		} catch (err) {
+			setError('Something went wrong!');
+			throw new Error('Something went wrong!');
+		}
+	};
 
-// 	useEffect(() => {
-// 		sendRequest();
-// 	}, [url]);
+	useEffect(() => {
+		sendRequest();
+	}, [url]);
 
-// 	return { data };
-// };
+	return { data };
+};
 
-// export default useFetch;
+export default useFetch;
