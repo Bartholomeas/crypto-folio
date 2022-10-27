2d0d8e583029363813c0e16bd03f26921ff38941
Bartholomeas <weivan321@gmail.com>
2022-10-27 18:26:31 +0200

    fixed(useFilter): nestings and props

--- components/atoms/FireIcon/FireIcon.tsx
+++ components/atoms/FireIcon/FireIcon.tsx
@@ -1,20 +1,20 @@
 import { AiFillFire } from "react-icons/ai";
 
 interface Props {
-  otherStyles?: string;
+	otherStyles?: string;
 }
 
 function FireIcon({ otherStyles }: Props) {
-  return (
-    <div
-      className={`absolute flex items-center justify-center w-fit
+	return (
+		<div
+			className={`absolute flex items-center justify-center w-fit
     ${otherStyles || ""}`}
-    >
-      <AiFillFire className="absolute fill-white text-[7rem]" />
-      <AiFillFire className="fill-orange-500 text-[5rem] animate-wiggleSlow " />
-      <AiFillFire className="absolute bottom-1 fill-yellow-400 text-[3rem] animate-wiggle" />
-    </div>
-  );
+		>
+			<AiFillFire className="dark:fill-dmBase absolute fill-white text-[7rem]" />
+			<AiFillFire className="fill-orange-500 text-[5rem] animate-wiggleSlow " />
+			<AiFillFire className="absolute bottom-1 fill-yellow-400 text-[3rem] animate-wiggle" />
+		</div>
+	);
 }
 
 export default FireIcon;
